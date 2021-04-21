80442d6802187ce618c3d3698b6ac596330e896b
pry0cc <pry0cc@0x00sec.org>
2021-04-21 03:07:23 +0100

    decreased timeout to 100 seconds for testing

--- wordlist-generate.py
+++ wordlist-generate.py
@@ -63,5 +63,5 @@ while True:
     save_file('relevant.txt', words, sort=True)
     save_file('relevant_lower.txt', words_lower, sort=True)
     commit_and_push()
-    print("Files saved! Waiting 1 hour until next search...")
-    time.sleep(3600)
+    print("Files saved! Waiting 100 seconds until next search...")
+    time.sleep(100)
